0f8316fcb028ca9a5922922ed3a21c8da63610dd
Gaurav Patil <patil6624@Gauravs-MacBook-Air.local>
2022-10-19 20:21:19 +0530

    Day 6 Assignment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using logicalprograming;
+using System;
+using System.Diagnostics;
+
+
+public class logicalProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Logical Program");
+        //logic.reverse();
+        //FibonacciSeries.fiboseries();
+        //PerfectNumber.Pnum();
+        // prime.prmnum();
+        //CouponNumber.checkCoupon();
+        // Stopwatch.SimulateStopwatch();
+        CouponNumber.checkCoupon();
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LogicalProgram
+{
+    public class Stopwatch
+    {
+        public Stopwatch()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Thread.Sleep(10000);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
+
